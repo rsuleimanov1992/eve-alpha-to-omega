fa65e3ccd06384fe3ef70853fd490d3db80b1386
Roman <rsuleimanov1992@gmail.com>
2025-09-07 18:26:36 +0300

    delete 1 file and update 1 file

--- eve-bot-framework/libeve/bots/ore_survey_priority_selector.py
+++ eve-bot-framework/libeve/bots/ore_survey_priority_selector.py
@@ -66,7 +66,7 @@ class OreSurveyPrioritySelectorBot(Bot):
     LASER_RANGE_M = 14990
     DIST_PENALTY_PER_KM = 0.03
     NUM_LASERS = 2
-    LASER_YIELD = 1500
+    LASER_YIELD = 1950
     LASER_CYCLE_SEC = 180
     SHIP_SPEED_MPS = 300
 
